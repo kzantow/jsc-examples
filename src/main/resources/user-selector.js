15c0bdc2ed434731fd3cda86e7536631a0ffa0ea
Keith Zantow <keith@zantow.us>
2015-05-17 18:53:52 -0400

    More examples

---
+++ src/main/resources/user-selector.js
@@ -0,0 +1,17 @@
+new function() {
+	this.userSearch = '';
+	
+	this.findUser = function() {
+		console.log('searching for ' + this.userSearch)
+		var users = userService.listUsers();
+		var out = [];
+		for(var i = 0; i < users.length; i++) {
+			if(users[i].name.contains(this.userSearch)) {
+				out.push(users[i]);
+			}
+		}
+		this.results = out;
+	};
+	
+	this.results = [];
+}
